27489d8bfbf64ef64568e79e73e79ec5b9fe21a6
Mohmansy94 <77630176+Mohmansy94@users.noreply.github.com>
2021-11-12 20:23:39 +0100

    Add files via upload

---
+++ Exercise 1.m
@@ -0,0 +1,76 @@
+%--------------------------------------------------------------------------
+%   
+%          ADJUSTMENT THEORY I
+%    Exercise 1: Introduction to Octave/Matlab   
+% 
+%   Author         : Anastasia Pasioti
+%   Version        : October 02, 2020
+%   Last changes   : October 02, 2021
+%
+%--------------------------------------------------------------------------
+
+clc;
+clear all;
+close all;
+
+%--------------------------------------------------------------------------
+%   Task 1
+%--------------------------------------------------------------------------
+% x = 
+% y = 
+% z = 
+% t = 
+
+%--------------------------------------------------------------------------
+%   Task 2 - Right angle triangle
+%--------------------------------------------------------------------------
+%Using the Law of Sines
+% a =                   %[m]
+% alpha =               %[degrees]
+% c =                   %[m] 
+
+% Using the Pythagorean Theorem   
+% b =                   %[m]
+% c_2 =                 %[m]
+
+%--------------------------------------------------------------------------
+%   Task 3 - General triangle
+%--------------------------------------------------------------------------
+%Using the Law of Cosines
+
+
+
+
+
+%Using the Law of Sines
+
+
+
+
+
+%--------------------------------------------------------------------------
+%   Task 4 - Using matrices
+%--------------------------------------------------------------------------
+% A = 
+% B = 
+
+%Calculate the product
+% C = 
+
+%Load file and assign matrix to N
+% N =
+
+%Calculate determinant, rank, inverse, pseudo-inverse, eigenvector and
+%eigenvalues of matrix N
+% det_N =
+% rn_N =
+
+
+
+
+
+
+
+
+
+
