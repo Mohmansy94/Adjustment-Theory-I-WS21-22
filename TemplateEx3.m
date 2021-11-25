dd845da7bce96bd31e7e57c509c3135a3acd4c96
Mohmansy94 <77630176+Mohmansy94@users.noreply.github.com>
2021-11-25 20:51:37 +0200

    Update TemplateEx3.m

--- TemplateEx3.m
+++ TemplateEx3.m
@@ -103,22 +103,39 @@ S_LL6 = [std_t^2,0; 0,std_t^2];
 diff_tend = 0.5*g*2*(t_end-t_0);
 diff_t0 = 0.5*g*2*(t_end-t_0)*(-1);
 var_H = (diff_tend^2)*(std_t^2) + (diff_t0^2)*(std_t^2);
-disp("the standard deviation of the height of the main building");
+disp("the standard deviation of the height");
 sH = vpa(sqrt(var_H),4)
 
 %--------------------------------------------------------------------------
 %   Task 7
 %--------------------------------------------------------------------------
 disp('Task 7')
-
 %Given
-
-
+x0 = 0; %unit m
+x = 1000; %unit m
+t0 = 0; %unit s
+v0 = 15; %unit m/s
+a = 2; %unit m/s^2
+std_v0 = 0.2;
+std_a = 0.1;
 %Time to travel 1km
-% t = 
-
+%x = x0 + v0*(t-t0) + 0.5*a*(t-t0)^2
+syms t;
+t = solve(x == x0 + v0*(t-t0) + 0.5*a*(t-t0)^2);
+disp("the time to travel 1km");
+t = t(t>0)
 %Standard deviation
-% st = 
+syms v0 a T;
+T = solve(x == x0 + v0*(T-t0) + 0.5*a*(T-t0)^2, T);
+T = T(2);
+%diff_v0 = diff(T,v0)
+%diff_a = diff(T,a)
+S_LL7 = [std_v0^2,0; 0,std_a^2];
+diff_v0 = subs(diff(T,v0),[v0,a],[15,2]);
+diff_a = subs(diff(T,a),[v0,a],[15,2]);
+var_t = (diff_v0^2)*(std_v0^2) + (diff_a^2)*(std_a^2);
+disp("the standard deviation");
+st = vpa(sqrt(var_t),4)
 
 
 
